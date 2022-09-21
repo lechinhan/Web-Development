34452d12f415d85dd751cdfdd7108d7bda0e776f
Nhan Le Nguyen Chi <lechinhan012@gmail.com>
2022-09-21 10:46:01 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -17,15 +17,15 @@ app.get("/", (req, res) => {
 });
 
 // handle 404 response
-// app.use((req, res, next) => {
-//     return next(new ApiError(404, "Resource not found"));
-// });
-// // define error-handling middleware last, after other app.use() and routes calls
-// app.use((err, req, res, next) => {
-//     return res.status(err.statusCode || 500).json({
-//         message: err.message || "Internal Server Error",
-//     });
-// });
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 app.use("/api/contacts", contactsRouter);
 module.exports = app;
